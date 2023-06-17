7e18bafa5f3d2887feef8f9d885918f6aa21fb80
Ahmed Zenbaa <135458675+Ahmed-Zenbaa@users.noreply.github.com>
2023-06-18 02:42:14 +0300

    Add files via upload

---
+++ roles/mysql/files/dump.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS demo (
+  message varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+INSERT INTO demo (message) VALUES("the PHP file file from inside Mysql DB works perfectly!!\n");
+
